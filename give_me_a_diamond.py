20024b4ae72b6e45c0118aff817fc2b524c131f9
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-04-16 11:45:42 +0530

    Create give_me_a_diamond.py

---
+++ give_me_a_diamond.py
@@ -0,0 +1,8 @@
+def diamond(n):
+    if n%2==0 or n<1:
+        return null
+    li = []
+    li += [''.join([' ' for _ in range(int((n-x)/2))]) + ''.join('*' for _ in range(x))+'\n' for x in range(n) if x%2==1]
+    li += [("{:^"+str(n)+"}").format(''.join(['*' for _ in range(n)]))+'\n']
+    li +=  [''.join([' ' for _ in range(int((n-x)/2))]) + ''.join('*' for _ in range(x))+'\n' for x in list(reversed(range(n))) if x%2==1]
+    return ''.join(li)
