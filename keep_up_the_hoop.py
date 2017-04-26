5b31d4fabdb7bbb727b62c71d943e542648cb328
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-04-26 11:26:53 +0530

    Create keep_up_the_hoop.py

---
+++ keep_up_the_hoop.py
@@ -0,0 +1,7 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Keep up the hoop
+#Problem level: 8 kyu
+
+def hoopCount(n):
+    return "Great, now move on to tricks" if n>=10 else "Keep at it until you get it"
