32f5e6ba519fbbbce8715a4ee8e709e8a758d613
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-04-26 10:16:17 +0530

    Create difference_of_volumes_of_cuboids.py

---
+++ difference_of_volumes_of_cuboids.py
@@ -0,0 +1,7 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Difference of Volumes of Cuboids
+#Problem level: 8 kyu
+
+def find_difference(a, b):
+    return abs((a[0]*a[1]*a[2])-(b[0]*b[1]*b[2]))
