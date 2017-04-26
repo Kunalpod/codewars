a1f218fd262153572bbc8df4196cd5f12abe0646
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-04-26 10:26:24 +0530

    Create basic_mathematical_operations.py

---
+++ basic_mathematical_operations.py
@@ -0,0 +1,7 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Basic Mathematical Operations
+#Problem level: 8 kyu
+
+def basic_op(operator, value1, value2):
+    return eval('value1'+operator+'value2')
