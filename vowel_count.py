86c86e6f89818915db182c66b331d9f3af024925
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-04-28 12:24:03 +0530

    Create vowel_count.py

---
+++ vowel_count.py
@@ -0,0 +1,7 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Vowel Count
+#Problem level: 7 kyu
+
+def getCount(inputStr):
+    return len([x for x in inputStr if x=='a' or x=='e' or x=='i' or x=='o' or x=='u'])
