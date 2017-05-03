3cf8c262fcff2074496bb1193bdc9cdc70ab34f5
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-05-03 16:22:57 +0530

    Update equal_sides_of_an_array.py

--- equal_sides_of_an_array.py
+++ equal_sides_of_an_array.py
@@ -4,9 +4,7 @@
 #Problem level: 6 kyu
 
 def find_even_index(arr):
-    if not sum(arr[1:]): return 0
-    if not sum(arr[:len(arr)-1]): return len(arr)-1
-    for i in range(1, len(arr)-1):
+    for i in range(0, len(arr)):
         if sum(arr[:i])==sum(arr[i+1:]):
             return i    
-    return -1        
+    return -1
