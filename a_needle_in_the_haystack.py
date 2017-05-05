3621ae0bdf3f738e366841c2ed905990c53ecaec
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-05-05 14:36:11 +0530

    Create a_needle_in_the_haystack.py

---
+++ a_needle_in_the_haystack.py
@@ -0,0 +1,7 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: A Needle in the Haystack
+#Problem level: 8 kyu
+
+def find_needle(haystack):
+    return "found the needle at position "+str(haystack.index('needle'))
