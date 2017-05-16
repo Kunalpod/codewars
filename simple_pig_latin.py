3d1a3f6e9f6abeea0c95abbeee9e8ffa2d06e436
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-05-16 16:26:28 +0530

    Create simple_pig_latin.py

---
+++ simple_pig_latin.py
@@ -0,0 +1,8 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Simple Pig Latin
+#Problem level: 6 kyu
+
+def pig_it(text):
+    text1 = ' '.join([x[1:]+x[0]+'ay' for x in text.split()])
+    return text1 if text1[-3].isalpha() else text1[:-2]
