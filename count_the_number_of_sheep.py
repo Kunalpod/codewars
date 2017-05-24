044686fe8f480d61de8556694914837d654153e0
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-05-24 15:17:42 +0530

    Create count_the_number_of_sheep.py

---
+++ count_the_number_of_sheep.py
@@ -0,0 +1,6 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Count all the sheep on farm in the heights of New Zealand
+#Problem level: 7 kyu
+
+def lostSheep(friday,saturday,total):    return total - sum(friday) - sum(saturday)
