fa3d2141eb8e3e55b41eadb62cfd5ea68356b5e7
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-05-25 09:39:31 +0530

    Create unfinished_loop_bug_fixing_#1.py

---
+++ unfinished_loop_bug_fixing_#1.py
@@ -0,0 +1,12 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Unfinished Loop - Bug Fixing #1
+#Problem level: 8 kyu
+
+def create_array(n):
+    res=[]
+    i=1
+    while i<=n: 
+        res+=[i]
+        i += 1
+    return res
