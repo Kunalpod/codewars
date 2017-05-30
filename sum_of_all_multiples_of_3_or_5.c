fe737a570e776b9ca3285e3aace98d102552a72f
Kunal Gautam <Kunalpod@users.noreply.github.com>
2017-05-30 16:08:56 +0530

    Create sum_of_all_multiples_of_3_or_5.c

---
+++ sum_of_all_multiples_of_3_or_5.c
@@ -0,0 +1,12 @@
+#Kunal Gautam
+#Codewars : @Kunalpod
+#Problem name: Sum of all the multiples of 3 or 5
+#Problem level: 7 kyu
+
+int findSum (int n){
+  int count = 0;
+  for(int i=1; i<=n; i++)
+    if(i%3==0||i%5==0)
+      count = count + i;
+  return(count);
+}
